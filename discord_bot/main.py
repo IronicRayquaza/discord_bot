7e9c1fc65dd3944b7471d6bb9eb3236ed89527d7
Satyam Singh <103063834+IronicRayquaza@users.noreply.github.com>
2024-03-24 01:56:08 +0530

    not working yet

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,63 +1,21 @@
-from typing import Final
-import os
-from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from responses import get_response
+import discord
+TOKEN="insert your token"
 
-# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
-load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+bot = discord.Bot()
 
-# STEP 1: BOT SETUP
-intents: Intents = Intents.default()
-intents.message_content = True  # NOQA
-client: Client = Client(intents=intents)
+# we need to limit the guilds for testing purposes
+# so other users wouldn't see the command that we're testing
 
+@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
+async def ping(ctx): # a slash command will be created with the name "ping"
+    await ctx.respond(f"Pong! Latency is {bot.latency}")
 
-# STEP 2: MESSAGE FUNCTIONALITY
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print('(Message was empty because intents were not enabled probably)')
-        return
-
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
-
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+bot.run("TOKEN")
 
 
-# STEP 3: HANDLING THE STARTUP FOR OUR BOT
-@client.event
-async def on_ready() -> None:
-    print(f'{client.user} is now running!')
-
-
-# STEP 4: HANDLING INCOMING MESSAGES
-@client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return
-
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
-
-
-# STEP 5: MAIN ENTRY POINT
-def main() -> None:
-    client.run(token=TOKEN)
-
-
-if __name__ == '__main__':
-    main()
-
+#not running yet 
+#yet to get this done right
+#yet to get the discord dev badge
 
 
 
