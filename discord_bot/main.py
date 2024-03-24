4bd2bb452a455339ba465eab6acc4a5477a20754
Satyam Singh <103063834+IronicRayquaza@users.noreply.github.com>
2024-03-24 15:55:08 +0530

    Update main.py

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,65 +1,14 @@
-from typing import Final
-import os
-from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from responses import get_response
+import discord
+bot=discord.Bot()
 
-# STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
-load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+@bot.event
+async def on_ready():
+    print("logged in as {bot.user}.")
 
-# STEP 1: BOT SETUP
-intents: Intents = Intents.default()
-intents.message_content = True  # NOQA
-client: Client = Client(intents=intents)
+testingservers=[insert your testserver client]
 
+@bot.slash_command(guide_ids=testingservers,name="work",description="checks to see if user online")
+async def work(ctx):
+    await ctx.respond(f"I am working! \n\nLatency: {bot.latency*1000} ms.")
 
-# STEP 2: MESSAGE FUNCTIONALITY
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print('(Message was empty because intents were not enabled probably)')
-        return
-
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
-
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
-
-
-# STEP 3: HANDLING THE STARTUP FOR OUR BOT
-@client.event
-async def on_ready() -> None:
-    print(f'{client.user} is now running!')
-
-
-# STEP 4: HANDLING INCOMING MESSAGES
-@client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return
-
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
-
-
-# STEP 5: MAIN ENTRY POINT
-def main() -> None:
-    client.run(token=TOKEN)
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
+bot.run('insert your token')
